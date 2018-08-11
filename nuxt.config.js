88db77b107d4c00a8304db691ad10ef1ae4da2a3
Ryan <ryan.holton@stsonline.uk.com>
2018-08-11 18:10:32 +0100

    Init

---
+++ nuxt.config.js
@@ -0,0 +1,45 @@
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/rocket-css/'
+  }
+} : {}
+module.exports = {
+  routerBase,
+  /*
+  ** Headers of the page
+  */
+  mode: 'spa',
+  head: {
+    title: 'rocket-css',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'The home of the open source lightweight Rocket CSS framework.' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
