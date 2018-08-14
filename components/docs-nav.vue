b8ca844719a6e0a30b4427714887a06a8746bce1
Ryan <ryan.holton@stsonline.uk.com>
2018-08-14 21:02:58 +0100

    Added flex, helpers, typography and visibility docs

--- components/docs-nav.vue
+++ components/docs-nav.vue
@@ -36,7 +36,19 @@
             <nuxt-link to="/docs/utilities/colours" class="rkt-docs-item rkt-text-secondary rkt-d-block rkt-p-y-1" active-class="rkt-docs-item-active" exact>Colours</nuxt-link>
           </li>
           <li>
+            <nuxt-link to="/docs/utilities/flex" class="rkt-docs-item rkt-text-secondary rkt-d-block rkt-p-y-1" active-class="rkt-docs-item-active" exact>Flex</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/docs/utilities/helpers" class="rkt-docs-item rkt-text-secondary rkt-d-block rkt-p-y-1" active-class="rkt-docs-item-active" exact>Helpers</nuxt-link>
+          </li>
+          <li>
             <nuxt-link to="/docs/utilities/spacing" class="rkt-docs-item rkt-text-secondary rkt-d-block rkt-p-y-1" active-class="rkt-docs-item-active" exact>Spacing</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/docs/utilities/typography" class="rkt-docs-item rkt-text-secondary rkt-d-block rkt-p-y-1" active-class="rkt-docs-item-active" exact>Typography</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/docs/utilities/visibility" class="rkt-docs-item rkt-text-secondary rkt-d-block rkt-p-y-1" active-class="rkt-docs-item-active" exact>Visibility</nuxt-link>
           </li>
         </ul>
       </div>
