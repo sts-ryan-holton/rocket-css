72588e106c126182262f2d65c985f0e256358ab6
Ryan <ryan.holton@stsonline.uk.com>
2018-10-21 12:46:09 +0100

    Version 1.0.1

--- pages/docs/index.vue
+++ pages/docs/index.vue
@@ -16,6 +16,19 @@
     </section>
     <div class="rkt-row rkt-m-t-2">
       <div class="rkt-col rkt-p-x-2">
+        <h2 class="rkt-font-weight-bold rkt-m-y-0">NPM</h2>
+        <p class="rkt-text-dark rkt-m-b-0">
+          Get up and running with Rocket CSS by installing through NPM.
+        </p>
+        <figure class="rkt-highlight">
+          <code>
+            npm install rocket-css --save
+          </code>
+        </figure>
+      </div>
+    </div>
+    <div class="rkt-row rkt-m-t-2">
+      <div class="rkt-col rkt-p-x-2">
         <h2 class="rkt-font-weight-bold rkt-m-y-0">CSS</h2>
         <p class="rkt-text-dark rkt-m-b-0">
           The easiest way to get up and running with Rocket CSS is to download Rocket CSS and include either our minified, or non-minified version of Rocket CSS.
